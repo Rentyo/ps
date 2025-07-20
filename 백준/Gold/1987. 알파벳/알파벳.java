6536964ff435c575283b78f9a1e74f019fe155db
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-07-20 22:50:00 +0900

    [Gold IV] Title: 알파벳, Time: 208 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main{
     static int max;
     static char[][] map;
+    static int[][] visited;
     static final int[] dCol = {1, 0, -1, 0};
     static final int[] dRow = {0, 1, 0, -1};
     static int r;
@@ -14,6 +15,7 @@ public class Main{
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         map = new char[r][c];
+        visited = new int[r][c];
         max = 1;
         for(int i = 0; i < r; i++){
             String s= br.readLine();
@@ -21,7 +23,7 @@ public class Main{
                 map[i][j] = s.charAt(j);
             }
         }
-
+        visited[0][0] = 1 << map[0][0] -'A';
         dfs(0, 0, 1, 1 << map[0][0] -'A');
         System.out.print(max);
     }
@@ -30,12 +32,17 @@ public class Main{
             max = 26;
             return;
         }
+        visited[col][row] = visit;
         boolean check = false;
         for(int i = 0; i < 4; i++){
             int nextCol = col + dCol[i];
             int nextRow = row + dRow[i];
             if(nextCol < 0 || nextCol == r || nextRow < 0 || nextRow == c) continue;
             if((visit & (1 << map[nextCol][nextRow] -'A')) != 0) continue;
+            if ((visit | 1 << map[nextCol][nextRow] - 'A') == visited[nextCol][nextRow]) {
+                continue;
+            }
+            visited[nextCol][nextRow] = visit | (1 << map[nextCol][nextRow] -'A');
             check = true;
             dfs(nextCol, nextRow, count+1, visit | (1 << map[nextCol][nextRow] -'A') );
         }

582a928273b91003a690cbc69f0e4ead6c161665
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-07-20 22:44:15 +0900

    [Gold IV] Title: 알파벳, Time: 808 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.util.*;
 public class Main{
-    public static final int[] dCol = {1, 0, -1, 0};
-    public static final int[] dRow = {0, 1, 0, -1};
-    public static char[][] map;
-    public static boolean[] visited;
-    public static int r;
-    public static int c;
-    public static int max = 0;
+    static int max;
+    static char[][] map;
+    static final int[] dCol = {1, 0, -1, 0};
+    static final int[] dRow = {0, 1, 0, -1};
+    static int r;
+    static int c;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -15,29 +14,35 @@ public class Main{
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
         map = new char[r][c];
-        visited = new boolean['Z'-'A' + 1];
-        for(int i = 0; i< r; i++){
-            String s = br.readLine();
+        max = 1;
+        for(int i = 0; i < r; i++){
+            String s= br.readLine();
             for(int j = 0; j < c; j++){
                 map[i][j] = s.charAt(j);
             }
         }
-        visited[map[0][0] -'A'] = true;
-        dfs(0, 0, 1);
+
+        dfs(0, 0, 1, 1 << map[0][0] -'A');
         System.out.print(max);
     }
-    public static void dfs(int col, int row, int count){
-        max = Math.max(max,count);
-        for(int i  = 0; i < 4; i++){
+    static void dfs(int col, int row, int count, int visit){
+        if(count == 26){
+            max = 26;
+            return;
+        }
+        boolean check = false;
+        for(int i = 0; i < 4; i++){
             int nextCol = col + dCol[i];
             int nextRow = row + dRow[i];
             if(nextCol < 0 || nextCol == r || nextRow < 0 || nextRow == c) continue;
-            else if(visited[map[nextCol][nextRow] - 'A']) continue;
-            visited[map[nextCol][nextRow] - 'A'] = true;
-            dfs(nextCol, nextRow, count+1);
-            visited[map[nextCol][nextRow] - 'A'] = false;
-
+            if((visit & (1 << map[nextCol][nextRow] -'A')) != 0) continue;
+            check = true;
+            dfs(nextCol, nextRow, count+1, visit | (1 << map[nextCol][nextRow] -'A') );
         }
+        if(!check){
+            max = Math.max(count,max);
+        }
+
 
     }
 }

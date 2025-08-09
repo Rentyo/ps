08e35632f287057aa6f39e7114627f800634386b
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-08-09 17:23:59 +0900

    [Silver II] Title: 부분수열의 합, Time: 72 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -3,7 +3,6 @@ import java.io.*;
 class Main {
     static int result;
     static int m;
-    static boolean[] v;
     static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
@@ -11,30 +10,21 @@ class Main {
 
         int n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        v = new boolean[n];
         arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for(int i =0; i<n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         result = 0;
-        powerset(0);
-        System.out.println(result);
+        powerset(0, 0);
+        System.out.println(m == 0 ? result -1 : result);
     }
-    public static void powerset(int index){
+    public static void powerset(int index, int sum){
         if(index == arr.length){
-            int sum = 0;
-            int count = 0;
-            for(int i = 0; i < arr.length;i++) {
-                sum+= v[i] ? arr[i] : 0;
-                count+= v[i] ? 1 : 0;
-            }
-            if(count!= 0 && sum == m) result++;
+            if(sum == m) result++;
             return;
         }
-        v[index] = true;
-        powerset(index+1);
-        v[index] = false;
-        powerset(index+1);
+        powerset(index+1, sum+arr[index]);
+        powerset(index+1, sum);
     }
 }

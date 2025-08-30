08fc6d381c3bdf50b3241916a076a3988ad4f456
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-08-30 19:49:15 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 972 ms, Memory: 96,204 KB -BaekjoonHub

---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    static class Processor{
+        int col, row;
+        boolean connected;
+        public Processor(int row, int col){
+            this.col = col;
+            this.row = row;
+        }
+    }
+    static int[][] d = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+    static int n;
+    static boolean[][] map;
+    static ArrayList<Processor> processors;
+    static Stack<int[]>[] stack;
+    // 프로세서의 최대 크기
+    static int max ;
+    // 선의 최소 길이
+    static int min ;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
+            n = Integer.parseInt(br.readLine());
+            map = new boolean[n][n];
+            processors = new ArrayList<Processor>();
+            for(int i = 0; i < n; i++){
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0 ; j < n; j++){
+                    if(Integer.parseInt(st.nextToken()) == 1){
+                        map[i][j] = true;
+                        processors.add(new Processor(i, j));
+                    }
+                }
+            }
+            stack = new Stack[processors.size()];
+            for(int i = 0; i < processors.size(); i++){
+                stack[i] = new Stack<>();
+            }
+            max = 0;
+            min = Integer.MAX_VALUE;
+            dfs(0, 0, 0);
+            sb.append(min).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void dfs(int index, int total, int count ){
+        if(index == processors.size()){
+            if(count > max){
+                max = count;
+                min = total;
+            }else if(count == max){
+                min = Math.min(min, total);
+            }
+            return;
+        }
+        for(int i = 0; i < 4; i++){
+            int length = check(index, i);
+            if(length != -1){
+                dfs(index +1, total+ length,  count + 1);
+            }
+            restore(index);
+        }
+        dfs(index+1, total, count);
+        restore(index);
+    }
+    public static void restore(int count){
+        while(stack[count].size() > 0){
+            int[] cur = stack[count].pop();
+            map[cur[0]][cur[1]] = false;
+        }
+    }
+    public static int check(int count, int dir){
+        int result = 0;
+        Processor p = processors.get(count);
+        if(p.row == n-1 || p.col == n-1 || p.row == 0 || p.col == 0) return -1;
+        int row = p.row;
+        int col = p.col;
+        while(true){
+            row +=d[dir][0];
+            col +=d[dir][1];
+            if(row < 0 || col < 0 || row >= n || col >= n) break;
+            if(map[row][col]) return -1;
+
+            stack[count].push(new int[]{row, col});
+            map[row][col] = true;
+            result++;
+        }
+        return result;
+    }
+}

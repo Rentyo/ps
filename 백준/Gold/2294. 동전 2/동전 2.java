7cedc9ec04fe9da26d883d61acb40b83702d8950
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-07 12:31:01 +0900

    [Gold V] Title: 동전 2, Time: 104 ms, Memory: 11784 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,28 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k =  Integer.parseInt(st.nextToken());
+        int[] dp = new int[k+1];
+        Arrays.fill(dp, 10001);
+        dp[0] = 0;
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++){
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coins);
+        for(int i = 1; i <= k; i++){
+            for(int j = 0; j < n; j++){
+                if(i - coins[j] < 0) break;
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        System.out.println(dp[k] != 10001 ? dp[k] : -1);
+    }
+}

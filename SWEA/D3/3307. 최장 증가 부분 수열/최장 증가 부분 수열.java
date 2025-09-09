8719bb1b1c193217e5fded4d9667a066e0bc6d38
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-09 11:37:21 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 87 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 class Solution
 {   
+    static ArrayList<Integer> dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -15,17 +16,30 @@ class Solution
             for(int i = 0; i < n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            int[] dp = new int[n];
-            int max = 1;
+            dp = new ArrayList<>();
+            dp.add(arr[0]);
             for(int i = 0; i < n; i++){
-                dp[i] = 1;
-                for(int j = 0; j < i; j++){
-                    if(arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+                if(arr[i] > dp.get(dp.size()-1)) dp.add(arr[i]);
+                else{
+                    int index = binary_search(arr[i]);
+                    dp.remove(index);
+                    dp.add(index, arr[i]);
                 }
-                max = Math.max(max, dp[i]);
             }
-            sb.append(max).append("\n");
+            sb.append(dp.size()).append("\n");
         }
         System.out.println(sb);
+    }
+    public static int binary_search(int target){
+        int start = 0;
+        int end = dp.size()-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(dp.get(mid) > target) end = mid-1;
+            else if(dp.get(mid) < target) start = mid+1;
+            else return mid;
+        }
+        return start;
     }
 }

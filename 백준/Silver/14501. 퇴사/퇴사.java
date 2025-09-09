5bd4e6eb127abaf4d410e1f647ffd7abf7affafd
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-09 22:17:34 +0900

    [Silver III] Title: 퇴사, Time: 68 ms, Memory: 11580 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // 0 은 상담 날짜 1은 value
+        int[][] counsels = new int[2][n];
+        StringTokenizer st;
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            counsels[0][i] = Integer.parseInt(st.nextToken());
+            counsels[1][i] = Integer.parseInt(st.nextToken());
+            if(i + counsels[0][i] - 1 < n) dp[i] = counsels[1][i];
+        }
+        for(int i = n-1; i >= 0; i--){
+            if(i + counsels[0][i] < n) dp[i] = Math.max(dp[i+counsels[0][i]]+counsels[1][i]  , dp[i]);
+            if(i + 1 < n) dp[i] =  Math.max(dp[i+1], dp[i]);
+        }
+        System.out.println(dp[0]);
+
+    }
+}

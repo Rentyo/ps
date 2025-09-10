e1be3376e54ebde4398faba8194ec74b05a45869
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-10 10:26:55 +0900

    [Gold III] Title: 내리막 길, Time: 276 ms, Memory: 38776 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -13,7 +13,6 @@ class Main
     static int m;
     static int[][] map;
     static int[][] dp;
-    static boolean[][] v;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,30 +24,26 @@ class Main
             st = new StringTokenizer(br.readLine());
             for(int j = 0 ; j < m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
-        v = new boolean[n][m];
         dfs(0, 0);
         System.out.println(dp[0][0]);
     }
     public static int dfs(int row, int col){
 
         if(row == n-1 && col == m-1){
-            v[row][col] = true;
             return 1;
         }
+        if(dp[row][col] != -1) return dp[row][col];
 
         int sum = 0;
         for(int i = 0; i < 4; i++){
             int nR = row + d[i][0];
             int nC = col + d[i][1];
             if(nR < 0 || nC < 0 || nR >= n || nC >= m || map[row][col] <= map[nR][nC]) continue;
-            if(v[nR][nC]) sum += dp[nR][nC];
-            else{
-                sum += dp[nR][nC] = dfs(nR, nC);
-            }
+            sum += dfs(nR, nC);
         }
-        v[row][col] = true;
         return dp[row][col] = sum;
     }
 }

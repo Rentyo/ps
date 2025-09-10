9dbed13c133edc7a79ddf4ab859c0a3328116eb6
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-10 14:17:40 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 14,829 ms, Memory: 103,268 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -7,47 +7,46 @@ class Solution
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-         StringTokenizer st;
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         for(int tc = 1; tc <= T; tc++){
             sb.append("#").append(tc).append(" ");
             st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
-            int[][] dp = new int[n][n];
+            ArrayList<Integer>[] arr = new ArrayList[n];
             for(int i = 0; i < n; i++){
+                arr[i] = new ArrayList<>();
                 for(int j = 0; j < n; j++){
-                    int now = Integer.parseInt(st.nextToken());
-                    if(i != j){
-                        if(now == 1) dp[i][j] = 1;
-                        else dp[i][j] = maxValue;
-                    }
+                    int num = Integer.parseInt(st.nextToken());
+                    if(num == 0) continue;
+                    arr[i].add(j);
                 }
             }
-
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(dp[j][i] == maxValue) continue;
-                    for(int k = 0; k < n; k++){
-                        if(dp[i][k] == maxValue || j == k) continue;
-                        dp[j][k] = Math.min(dp[j][k], dp[j][i] + dp[i][k]);
-                    }
-                }
-            }
+            Queue<int[]> queue = new ArrayDeque<>();
+            boolean[] v;
             int min = maxValue;
             for(int i = 0; i < n; i++){
-                boolean check = false;
                 int sum = 0;
-                for(int j = 0; j < n; j++){
-                    if(dp[i][j] == maxValue){
-                        check = true;
-                        break;
+                v = new boolean[n];
+                queue.offer(new int[]{i, 0});
+                int count = 1;
+                v[i] = true;
+                while(queue.size() > 0){
+                    int[] cur = queue.poll();
+                    for(int j = 0; j < arr[cur[0]].size(); j++){
+                        int next = arr[cur[0]].get(j);
+                        if(v[next]) continue;
+                        v[next] = true;
+                        queue.offer(new int[]{next, cur[1] + 1});
+                        count++;
+                        sum+= cur[1] + 1;
                     }
-                    sum += dp[i][j];
                 }
-                if(!check) min = Math.min(min, sum);
+                if(count == n) min = Math.min(sum, min);
             }
             sb.append(min).append("\n");
         }
+        br.close();
         System.out.println(sb);
     }
 }

6fed3adfe9ea79691866248adb4628a4963e53a0
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-12 11:57:24 +0900

    [D4] Title: 사랑의 카운슬러, Time: 143 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
+++ SWEA/D4/1494. 사랑의 카운슬러/사랑의 카운슬러.java
@@ -1,58 +1,53 @@
 import java.util.*;
 import java.io.*;
-
+ 
 class Solution
-{
-    public static boolean[] visited;
-    public static int n;
-    public static int[][] map;
-    public static Long min;
-	public static void main(String args[]) throws Exception
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T=Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
+{   
+    static long totalX;
+    static long totalY;
+    static int n; 
+    static int[][] map;
+    static long min;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T=Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
             sb.append("#").append(test_case).append(" ");
             n = Integer.parseInt(br.readLine());
-            visited = new boolean[n];
             map = new int[n][2];
+            totalX = 0;
+            totalY = 0;
             for(int i = 0; i < n; i++){
-            	st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                map[i][0] = x;
-                map[i][1] = y;
+                st = new StringTokenizer(br.readLine());
+                map[i][0] = Integer.parseInt(st.nextToken());
+                totalX += map[i][0];
+                map[i][1] = Integer.parseInt(st.nextToken());
+                totalY += map[i][1];
             }
-            min =   Long.MAX_VALUE;
-            dfs(0, 0);
-			sb.append(min).append("\n");
+            min =  Long.MAX_VALUE;
+            comb(0, 0, 0, 0);
+            sb.append(min).append("\n");
         }
         System.out.print(sb);
-	}
-    public static void dfs(int count, int index){
-    	if(count == n/2){
-            int totalX = 0;
-            int totalY = 0;
-        	for(int i = 0; i < n; i++){
-            	if(visited[i]){
-                	totalX += map[i][0];
-                    totalY += map[i][1];
-                }else{
-                	totalX -= map[i][0];
-                    totalY -= map[i][1];
-                }
-            }
-            min = Math.min((long)totalX*totalX + (long)totalY*totalY, min);
+    }
+    public static void comb(int count, int index, long sumX, long sumY){
+        if(count == n/2){
+            long restX = totalX - sumX;
+            long restY = totalY - sumY;
+            long calX = sumX - restX;
+            long calY = sumY - restY;
+            min = Math.min(calX*calX + calY*calY, min);
             return;
         }
-        for(int i = index; i < n; i++){
-            visited[i] = true;
-            dfs(count+1, i+1);
-            visited[i] = false;
-        }
+
+        if(index == n) return;
+
+        comb(count+1, index + 1, sumX + map[index][0], sumY + map[index][1]);
+        comb(count, index+1, sumX, sumY);
     }
 }

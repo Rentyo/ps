58620b2507233d02e891274f5a4e67372ff1f161
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-12 11:01:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 367 ms, Memory: 104,420 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -13,18 +13,21 @@ public class Solution {
         int col;
         int life;
         int now;
-        public Cell(int row, int col, int life){
+        int time;
+        public Cell(int row, int col, int life, int time){
             this.row = row;
             this.col = col;
             this.life = life;
             this.now = 0;
+            this.time = time;
         }
         @Override
         public int compareTo(Cell o){
-            return -1*Integer.compare(this.life, o.life);
+            if(this.time == o.time) return -1*Integer.compare(this.life, o.life);
+            else return Integer.compare(this.time, o.time);
         }
     }
-    static Queue<Cell> cells;
+    static PriorityQueue<Cell> cells;
     static int[][] map;
     static int size;
     public static void main(String[] args) throws IOException{
@@ -40,13 +43,13 @@ public class Solution {
             int inputrow = Integer.parseInt(st.nextToken());
             int inputcol = Integer.parseInt(st.nextToken());
             int time = Integer.parseInt(st.nextToken());
-            cells = new ArrayDeque<Cell>();
+            cells = new PriorityQueue<Cell>();
             for(int i = 500 ; i < 500 + inputrow; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j = 500; j < 500 + inputcol; j++){
                     map[i][j] = Integer.parseInt(st.nextToken());
                     if(map[i][j] > 0){
-                        cells.offer(new Cell(i, j, map[i][j]));
+                        cells.offer(new Cell(i, j, map[i][j],1));
                         map[i][j] = 1;
                     }
                 }
@@ -57,33 +60,28 @@ public class Solution {
         System.out.print(sb);
     }
     static void simulate(int time){
-        PriorityQueue<Cell> pq = new PriorityQueue<>();
-        Queue<Cell> temp = new ArrayDeque<>();
-        for(int i = 1; i <= time; i++){
-            while(cells.size() > 0){
-                Cell cur = cells.poll(); 
-                cur.now++;
-                if(cur.now == 1 && map[cur.row][cur.col] == 2){
-                    for(int j = 0; j < 4; j++){
-                        int nR = cur.row + d[j][0];
-                        int nC = cur.col + d[j][1];
-                        if(map[nR][nC] != 0) continue;
-                        pq.offer(new Cell(nR, nC, cur.life));
-                    }
-                }
-                if(cur.now == cur.life){
-                    cur.now = 0;
-                    map[cur.row][cur.col]++;
-                }
-                if(map[cur.row][cur.col] != 3) temp.offer(cur);
-            }
-            while(pq.size() > 0){
-                Cell cur = pq.poll();
-                if(map[cur.row][cur.col] > 0) continue;
+        while(cells.size() > 0){
+            Cell cur = cells.poll(); 
+            cur.now++;
+            if(cur.time > time) {
                 cells.offer(cur);
-                map[cur.row][cur.col] = 1;
+                break;
             }
-            while(temp.size() > 0) cells.offer(temp.poll());
+            cur.time++;
+            if(cur.now == 1 && map[cur.row][cur.col] == 2){
+                for(int j = 0; j < 4; j++){
+                    int nR = cur.row + d[j][0];
+                    int nC = cur.col + d[j][1];
+                    if(map[nR][nC] != 0) continue;
+                    cells.offer(new Cell(nR, nC, cur.life, cur.time));
+                    map[nR][nC] = 1;
+                }
+            }
+            if(cur.now == cur.life){
+                cur.now = 0;
+                map[cur.row][cur.col]++;
+            }
+            if(map[cur.row][cur.col] != 3) cells.offer(cur);
         }
     }
 }

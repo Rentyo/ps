18f2fa86b88f1f9d8eabc8548344d488ca7d6c11
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-16 20:42:04 +0900

    [Silver I] Title: 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유, Time: 1128 ms, Memory: 337064 KB -BaekjoonHub

---
+++ 백준/Silver/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static int[][] d = {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0}
+    };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        for(int i = 0; i < N; i++){
+            String s = br.readLine();
+            for(int j = 0; j < M; j++){
+                map[i][j] = s.charAt(j) - '0';
+                if(map[i][j] == 2) {
+                    queue.offer(new int[]{i, j, 0});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int result = -1;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+
+            if(map[cur[0]][cur[1]] == 3 || map[cur[0]][cur[1]] == 4 || map[cur[0]][cur[1]] == 5 ) {
+                result = cur[2];
+                break;
+            }
+
+            for(int i = 0; i < 4; i++){
+                int nR = cur[0] + d[i][0];
+                int nC = cur[1] + d[i][1];
+
+                if(nR < 0 || nR >= N || nC < 0 || nC >= M || visited[nR][nC] || map[nR][nC] == 1) continue;
+                visited[nR][nC] = true;
+                queue.offer(new int[]{nR, nC, cur[2] +1});
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if(result == -1){
+            sb.append("NIE");
+        }else{
+            sb.append("TAK").append("\n").append(result);
+        }
+        System.out.println(sb);
+
+    }
+}

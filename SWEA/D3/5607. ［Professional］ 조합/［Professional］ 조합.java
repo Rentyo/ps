ebb75e7e5cb601b7d20a58e45fed53854d5f9fe7
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-16 14:44:59 +0900

    [D3] Title: [Professional] 조합, Time: 94 ms, Memory: 33,792 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.java
@@ -1,12 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Solution {
+    static long[] fact;
     static final int mod = 1234567891;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+
+        fact = new long[1000001];
+        fact[0] = 1;
+        long result = 1;
+        for(int i = 1; i <= 1000000; i++){
+            fact[i] = result = result* i % mod;
+        }
+
+
         for(int test_case = 1; test_case <= t; test_case++) {
             sb.append("#").append(test_case).append(" ");
             st = new StringTokenizer(br.readLine());
@@ -14,8 +24,8 @@ public class Solution {
             int N = Integer.parseInt(st.nextToken());
             int R = Integer.parseInt(st.nextToken());
 
-            long result = factorial(N) * divide_merge(factorial(N-R) * factorial(R) % mod , mod-2) % mod;
-            sb.append(result).append("\n");
+            long cal = fact[N] * divide_merge(fact[N-R] * fact[R] % mod , mod-2) % mod;
+            sb.append(cal).append("\n");
         }
         System.out.println(sb);  
     }
@@ -26,15 +36,6 @@ public class Solution {
         long result = d * d % mod;
         if(b % 2 == 1) result = result * a % mod;
         return result;
-    }
-    public static long factorial(int n){
-        
-        long total = 1;
-        for(int i = 2; i <= n; i++){
-            total = i * total % mod;
-        }
-
-        return total;
     }
 }
      

4d2149ad3aa4b87f991086d8755341565962df03
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-21 10:54:25 +0900

    [Gold III] Title: 중량제한, Time: 352 ms, Memory: 49396 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,55 +1,58 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static ArrayList<int[]>[] map;
-    static boolean[] v;
+    static int[] root;
     static int sP;
     static int eP;
-    public static void main(String[] args) throws   IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int max = 0;
-        map = new ArrayList[n];
-        for(int i = 0; i < n; i++) {
-            map[i] = new  ArrayList<>();
-        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return -1*Integer.compare(o1[2], o2[2]);
+            }
+        });
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
             int node1 = Integer.parseInt(st.nextToken())-1;
             int node2 = Integer.parseInt(st.nextToken())-1;
             int value = Integer.parseInt(st.nextToken());
             max = Math.max(max, value);
-            map[node1].add(new int[]{node2, value});
-            map[node2].add(new int[]{node1, value});
+            pq.offer(new int[]{node1, node2, value});
         }
-        v = new boolean[n];
+        root = new int[n];
+        for(int i = 0; i < n; i++){
+            root[i] = i;
+        }
         st  = new StringTokenizer(br.readLine());
         sP =  Integer.parseInt(st.nextToken())-1;
         eP = Integer.parseInt(st.nextToken())-1;
-        System.out.println(binary_search(0, max));
-    }
 
-    public static int binary_search(int start, int end){
-        if(start > end) return end;
-        int mid = start + (end-start)/2;
-        v = new boolean[v.length];
-        v[sP] = true;
-        if(dfs(sP, mid)) return binary_search(mid+1, end);
-        else return binary_search(start, mid-1);
-    }
-    public static boolean dfs(int node, int value){
-        if(node == eP) return true;
-        for(int i = 0; i < map[node].size(); i++){
-            int nN = map[node].get(i)[0];
-            int nV = map[node].get(i)[1];
-            if(nV < value || v[nN]) continue;
-            v[nN] = true;
-            if(dfs(nN, value)) return true;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            if(find(cur[0]) == find(cur[1])) continue;
+            union(cur[0], cur[1]);
+            if(find(sP) == find(eP)) {
+                System.out.println(cur[2]);
+                break;
+            }
         }
-        return false;
+
+    }
+    public static void union(int a, int b){
+        int rA = find(a);
+        int rB = find(b);
+
+        if(rA > rB) root[rA] = rB;
+        else root[rB] = rA;
     }
 
-
+    public static int find(int a){
+        if(a == root[a]) return a;
+        return root[a] = find(root[a]);
+    }
 }

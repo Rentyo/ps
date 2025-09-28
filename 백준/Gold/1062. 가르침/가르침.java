421e433f77a55e784d48c2b9f848a47d7880cd71
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-09-28 23:15:34 +0900

    [Gold IV] Title: 가르침, Time: 168 ms, Memory: 12940 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
     static boolean[] v = new boolean[26];
-    static String[] words;
+    static int[] words;
     static int k;
     static int max = 0;
     public static void main(String[] args) throws IOException {
@@ -10,36 +10,32 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n =Integer.parseInt(st.nextToken());
         k= Integer.parseInt(st.nextToken());
-        words = new String[n];
+        words = new int[n];
         for(int i = 0; i < n; i++){
-            words[i] = br.readLine();
+            String s = br.readLine();
+            for(int j = 0; j < s.length(); j++){
+                words[i] |= (1 << s.charAt(j) - 'a');
+            }
         }
-        comb(0, 0);
+        comb(0, 0, 0);
         System.out.println(max);
     }
-    static void comb(int count, int index){
+    static void comb(int count, int index, int value){
         if(count == k){
-            max = Math.max(max, check());
+            max = Math.max(max, check(value));
         }
         if(index == 26) return;
         v[index] = true;
-        comb(count+1, index+1);
+        comb(count+1, index+1, value | (1 << index));
         v[index] = false;
         if(index == 'a'-'a' || index == 'c' - 'a' || index == 't'-'a' || index == 'i' -'a' || index == 'n' -'a') return;
-        comb(count, index+1);
+        comb(count, index+1, value);
     }
-    static int check(){
+    static int check(int value){
         int count = 0;
         boolean c;
         for(int i = 0; i < words.length; i++){
-            c= true;
-             for(int j = 4; j < words[i].length()-3; j++){
-                if( !v[words[i].charAt(j) - 'a'] ){
-                    c = false;
-                    break;
-                }
-            }
-            if(c) count++;
+            if((words[i] & value) == words[i]) count++;
         }
         return count;
     }

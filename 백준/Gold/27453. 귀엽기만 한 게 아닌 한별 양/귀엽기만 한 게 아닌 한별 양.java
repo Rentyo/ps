80a959381f0065796de55c36caf51c138a66f225
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-10-13 11:57:14 +0900

    [Gold I] Title: 귀엽기만 한 게 아닌 한별 양, Time: 576 ms, Memory: 151932 KB -BaekjoonHub

--- 백준/Gold/27453. 귀엽기만 한 게 아닌 한별 양/귀엽기만 한 게 아닌 한별 양.java
+++ 백준/Gold/27453. 귀엽기만 한 게 아닌 한별 양/귀엽기만 한 게 아닌 한별 양.java
@@ -1,84 +1,96 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    static int N;
-    static int M;
-    static int K;
-    static char[][] map;
-    public static int[][] d = {
-            {1, 0},
-            {-1, 0},
-            {0, 1},
-            {0, -1},
-    };
-
-    static class Pos {
-        int row;
-        int col;
+    public static class Pos {
+        int nowRow;
+        int nowCol;
         int preRow;
         int preCol;
         int time;
-
-        public Pos(int row ,int col, int preRow, int preCol, int time) {
-            this.time = time;
-            this.preCol = preCol;
+        public Pos(int nowRow, int nowCol, int preRow, int preCol, int time){
+            this.nowRow = nowRow;
+            this.nowCol = nowCol;
             this.preRow = preRow;
-            this.col = col;
-            this.row = row;
-
-
+            this.preCol = preCol;
+            this.time = time;
         }
     }
-    static HashSet<Integer> visited;
-
+    public static int[][] d = {
+        {-1, 0},
+        {0, 1},
+        {1, 0},
+        {0, -1}
+    };
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        map = new char[N][M];
-        visited = new HashSet<>();
-        Queue<Pos> queue = new ArrayDeque<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N][M];
+        
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = 0;
+        int endCol = 0;
         for(int i = 0; i < N; i++){
             String s = br.readLine();
             for(int j = 0; j < M; j++){
-                map[i][j] = s.charAt(j);
-                if(map[i][j] == 'S'){
-                    Pos pos = new Pos(i, j, i, j, 0);
-                    map[i][j] = '0';
-                    queue.offer(pos);
+                switch (s.charAt(j)){
+                    case 'H' : {
+                        endRow = i;
+                        endCol = j;
+                        map[i][j] = 0;
+                        break;
+                    } 
+                    case 'X' : {
+                        map[i][j] = -1;
+                        break;
+                    }
+                    case 'S' : {
+                        map[i][j] = 0;
+                        startRow = i;
+                        startCol = j;
+                        break;
+                    }
+                    default  : {
+                        map[i][j] = s.charAt(j) -'0';
+                        break;
+                    }
+
                 }
             }
         }
+        Queue<Pos> queue = new ArrayDeque<>();
+        int[][] visited = new int[N][M];
+        queue.offer(new Pos(startRow, startCol, startRow, startCol, 0));
 
-        while(!queue.isEmpty()){
-            Pos pos = queue.poll();
-            int row = pos.row;
-            int col = pos.col;
-            int preRow = pos.preRow;
-            int preCol = pos.preCol;
-            int time = pos.time;
-            int temp = (row * 1000 + col) * 1000000;
+        while(queue.size() > 0){
+            Pos now = queue.poll();
+            int pp = map[now.preRow][now.preCol];
+            int p = map[now.nowRow][now.nowCol];
+            for(int i = 0; i < 4; i++){
 
-
-            for(int i = 0; i < 4 ; i++){
-                int nR = row + d[i][0];
-                int nC = col + d[i][1];
-
-                if(nR < 0 || nC < 0 || nR >= N || nC >= M || (nR == preRow && nC == preCol) || map[nR][nC] =='X')continue;
-                if(map[nR][nC] == 'H'){
-                    System.out.println(time+1);
+                int nR = now.nowRow + d[i][0];
+                int nC = now.nowCol + d[i][1];
+                if(nR < 0 || nC < 0 || nR >= N || nC >= M || map[nR][nC] == -1 || (nR == now.preRow && nC == now.preCol)) continue;
+                if( (visited[nR][nC] & (1 << i)) != 0) continue;
+                if(nR == endRow && nC == endCol){
+                    System.out.println(now.time + 1);
                     return;
                 }
-                if(visited.contains(temp + nR * 1000 + nC)) continue;
-                if((map[preRow][preCol]-'0') + (map[row][col]-'0') + (map[nR][nC]-'0') > K) continue;
-                visited.add(temp + nR * 1000 + nC);
-                queue.offer(new Pos(nR, nC, row, col, time + 1));
+                
+                int nV = map[nR][nC];
+                if(pp + p + nV > K) continue;
+
+                visited[nR][nC] |= (1 << i);
+                queue.offer(new Pos(nR, nC, now.nowRow, now.nowCol, now.time+1));
+
             }
         }
         System.out.println(-1);
+        
     }
-
 }

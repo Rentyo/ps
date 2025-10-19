75407c8d0eec4b30a5d0e16aaa4dd3534342c47c
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-10-19 21:45:29 +0900

    [Gold III] Title: 나무 재테크, Time: 976 ms, Memory: 268264 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.java
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.java
@@ -18,7 +18,7 @@ public class Main {
     // 추가 양분
     static int[][] A;
     static int n;
-    static PriorityQueue<Integer>[][] pq;
+    static ArrayList<Integer>[][] trees;
 
     //현재까지의 나무 개수
     static int sum;
@@ -31,13 +31,13 @@ public class Main {
         value = new int[n][n];
         dead = new int[n][n];
         A = new int[n][n];
-        pq = new PriorityQueue[n][n];
+        trees = new ArrayList[n][n];
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < n; j++){
                 A[i][j] = Integer.parseInt(st.nextToken());
                 value[i][j] = 5;
-                pq[i][j] = new PriorityQueue<>();
+                trees[i][j] = new ArrayList<>();
             }
         }
 
@@ -46,7 +46,7 @@ public class Main {
             int row = Integer.parseInt(st.nextToken())-1;
             int col = Integer.parseInt(st.nextToken())-1;
             int age = Integer.parseInt(st.nextToken());
-            pq[row][col].offer(age);
+            trees[row][col].add(age);
         }
         sum = 0;
         for(int i = 1; i <= k; i++){
@@ -59,22 +59,20 @@ public class Main {
         System.out.println(sum);
     }
     public static void Spring(){
-        Queue<int[]> temp = new ArrayDeque<>();
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                while(!pq[i][j].isEmpty()){
-                    int age = pq[i][j].poll();
+                ArrayList<Integer> temp = new ArrayList<>();
+                Collections.sort(trees[i][j]);
+                for(int k = 0; k < trees[i][j].size(); k++){
+                    int age = trees[i][j].get(k);
                     if(age <= value[i][j]){
                         value[i][j] -= age;
-                        temp.offer(new int[]{i, j, age+1});
+                        temp.add(age+1);
                     }else{
                         dead[i][j] += (age/2);
                     }
                 }
-                while(temp.size() > 0){
-                    int[] tree = temp.poll();
-                    pq[tree[0]][tree[1]].offer(tree[2]);
-                }
+                trees[i][j] = temp;
             }
         }
     }
@@ -87,29 +85,30 @@ public class Main {
         }
     }
     public static void Fall(){
-        Queue<int[]> temp = new ArrayDeque<>();
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                while(pq[i][j].size() > 0) {
-                    int age = pq[i][j].poll();
-                    temp.offer(new int[]{i, j, age});
+                for(int k = 0; k < trees[i][j].size(); k++) {
+                    int age = trees[i][j].get(k);
                     if(age % 5 != 0){
                         continue;
                     }
-                    for (int k = 0; k < 8; k++) {
-                        int nI = i + d[k][0];
-                        int nJ = j + d[k][1];
+                    for (int l = 0; l < 8; l++) {
+                        int nI = i + d[l][0];
+                        int nJ = j + d[l][1];
                         if (nI < 0 || nI >= n || nJ < 0 || nJ >= n) continue;
-                        temp.offer(new int[]{nI, nJ, 1});
+                        trees[nI][nJ].add(1);
                     }
                 }
             }
         }
-        sum = temp.size();
-        while(temp.size() > 0){
-            int[] now = temp.poll();
-            pq[now[0]][now[1]].offer(now[2]);
+        int size = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                size += trees[i][j].size();
+            }
         }
+        sum = size;
+
     }
     public static void Winter(){
         for(int i = 0; i < n; i++){

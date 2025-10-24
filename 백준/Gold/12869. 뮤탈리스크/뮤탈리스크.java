b370234d1ff6e8aa9239fe65530aa90a324b0062
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-10-24 14:15:44 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 72 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    static final int[][] d2 = {{-9, -3} , {-3, -9}};
+    static final int[][] d3 = {{-9, -3, -1}, {-9, -1, -3}, {-3, -9, -1}, {-3, -1, -9} , {-1, -9, -3}, {-1, -3, -9}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -17,47 +19,55 @@ public class Main {
                 st = new StringTokenizer(br.readLine());
                 life1 = Integer.parseInt(st.nextToken());
                 life2 = Integer.parseInt(st.nextToken());
-                int[][] dp = new int[life1+1][life2+1];
-                for(int i = 0; i <= life1; i++){
-                    int preI1 = Math.max(i - 9 , 0);
-                    int preI2 = Math.max(i - 3, 0);
-                    for(int j = 0; j <= life2; j++){
-                        if(i == 0 && j == 0) continue;
-                        int preJ1 = Math.max(j - 3, 0);
-                        int preJ2 = Math.max(j - 9, 0);
-                        dp[i][j] = Math.min(dp[preI1][preJ1], dp[preI2][preJ2]) + 1;
+                boolean[][] visited = new boolean[life1+1][life2+1];
+                Queue<int[]> queue = new ArrayDeque<>();
+                queue.offer(new int[]{life1, life2, 0});
+                visited[life1][life2] = true;
+
+                while(queue.size() > 0){
+                    int[] cur = queue.poll();
+
+                    for(int i = 0; i < d2.length; i++){
+                        int nextI = Math.max(cur[0] + d2[i][0] , 0);
+                        int nextJ = Math.max(cur[1] + d2[i][1] , 0);
+                        if(nextI == 0 && nextJ == 0) {
+                            System.out.println(cur[2] + 1);
+                            return;
+                        }
+                        if(visited[nextI][nextJ]) continue;
+                        visited[nextI][nextJ] = true;
+                        queue.offer(new int[]{nextI, nextJ, cur[2] + 1 });
                     }
+
                 }
-                System.out.println(dp[life1][life2]);
                 break;
             case 3 :
                 st = new StringTokenizer(br.readLine());
                 life1 = Integer.parseInt(st.nextToken());
                 life2 = Integer.parseInt(st.nextToken());
                 life3 = Integer.parseInt(st.nextToken());
-                int[][][] dp2 = new int[life1+1][life2+1][life3+1];
-                for(int i = 0; i <= life1; i++){
-                    int preI1 = Math.max(i - 9 , 0);
-                    int preI2 = Math.max(i - 3, 0);
-                    int preI3 = Math.max(i - 1, 0);
-                    for(int j = 0; j <= life2; j++){
-                        int preJ1 = Math.max(j - 9 , 0);
-                        int preJ2 = Math.max(j - 3, 0);
-                        int preJ3 = Math.max(j - 1, 0);
-                        for(int k = 0; k <= life3; k++){
-                            if(i == 0 && j == 0 && k == 0) continue;
-                            int preK1 = Math.max(k - 9 , 0);
-                            int preK2 = Math.max(k - 3, 0);
-                            int preK3 = Math.max(k - 1, 0);
-                            dp2[i][j][k] = Math.min(dp2[preI1][preJ2][preK3] , dp2[preI1][preJ3][preK2]) + 1;
-                            dp2[i][j][k] = Math.min(dp2[i][j][k] , dp2[preI2][preJ1][preK3] + 1);
-                            dp2[i][j][k] = Math.min(dp2[i][j][k] , dp2[preI2][preJ3][preK1] + 1);
-                            dp2[i][j][k] = Math.min(dp2[i][j][k] , dp2[preI3][preJ1][preK2] + 1);
-                            dp2[i][j][k] = Math.min(dp2[i][j][k] , dp2[preI3][preJ2][preK1] + 1);
+                boolean[][][] visited2 = new boolean[life1+1][life2+1][life3 +1];
+                Queue<int[]> queue2 = new ArrayDeque<>();
+                queue2.offer(new int[]{life1, life2,life3 ,0});
+                visited2[life1][life2][life3] = true;
+
+                while(queue2.size() > 0){
+                    int[] cur = queue2.poll();
+
+                    for(int i = 0; i < d3.length; i++){
+                        int nextI = Math.max(cur[0] + d3[i][0] , 0);
+                        int nextJ = Math.max(cur[1] + d3[i][1] , 0);
+                        int nextK = Math.max(cur[2] + d3[i][2] , 0);
+                        if(nextI == 0 && nextJ == 0 && nextK == 0) {
+                            System.out.println(cur[3] + 1);
+                            return;
                         }
+                        if(visited2[nextI][nextJ][nextK]) continue;
+                        visited2[nextI][nextJ][nextK] = true;
+                        queue2.offer(new int[]{nextI, nextJ, nextK ,cur[3] + 1 });
                     }
+
                 }
-                System.out.println(dp2[life1][life2][life3]);
 
 
 

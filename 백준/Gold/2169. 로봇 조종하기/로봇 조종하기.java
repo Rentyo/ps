9f6264072d263f86768e836d68d4a2a9b59540f2
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-10-31 23:04:26 +0900

    [Gold II] Title: 로봇 조종하기, Time: 504 ms, Memory: 87776 KB -BaekjoonHub

---
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+public class Main {
+    static final int fixedValue = 1000 * 1000 * 100 * -1 - 1;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] map = new int[N][M];
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        // 현재 가려는 방향
+        int[][] dp = new int[N][M];
+        int[][] tmp = new int[N][M];
+        int[][] tmp2 = new int[N][M];
+        for(int i = 0; i < N; i++){
+            Arrays.fill(dp[i], fixedValue);
+            Arrays.fill(tmp[i] ,fixedValue);
+            Arrays.fill(tmp2[i],fixedValue);
+        }
+        dp[0][0] = map[0][0];
+        for(int j = 1; j < M; j++){
+            dp[0][j] = dp[0][j-1] + map[0][j];
+        }
+
+
+
+        for(int i = 1; i < N; i++){
+            for(int j = 0; j < M; j++){
+                tmp[i][j] = Math.max(dp[i-1][j], tmp[i][Math.max(j-1, 0)]) + map[i][j];
+            }
+            for(int j = M-1; j >= 0; j--){
+                tmp2[i][j] = Math.max(dp[i-1][j], tmp2[i][Math.min(j+1, M-1)])  + map[i][j];
+            }
+
+            for(int j = 0; j < M; j++){
+                dp[i][j] = Math.max(tmp[i][j], tmp2[i][j]);
+            }
+        }
+        System.out.println(dp[N-1][M-1]);
+
+
+
+
+
+
+    }
+}

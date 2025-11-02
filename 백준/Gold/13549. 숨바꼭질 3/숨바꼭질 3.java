549807474b7461705fce93bd2c1379f0e97b9d97
oleveloP2R <67499042+Rentyo@users.noreply.github.com>
2025-11-02 18:20:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 96 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -4,55 +4,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Pos implements Comparable<Pos>{
-        int pos;
-        int time;
-        public Pos(int pos, int time){
-            this.pos = pos;
-            this.time = time;
-        }
-        @Override
-        public int compareTo(Pos o) {
-            return Integer.compare(this.time, o.time);
-        }
-
-
-    }
+    static int[] v;
+    static int M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] v = new int[100_001];
+        M = Integer.parseInt(st.nextToken());
+        v = new int[100_001];
         Arrays.fill(v, 100_002);
-        PriorityQueue<Pos> q = new PriorityQueue<>();
-        q.offer(new Pos(N, 0));
 
-        while(!q.isEmpty()){
-            Pos p = q.poll();
-            if(p.time >= v[p.pos]) continue;
-            v[p.pos] = p.time;
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[]{N, 0});
+        v[N] = 0;
+        while(dq.size() > 0){
+            int[] cur =  dq.pollFirst();
 
-
-            if(p.pos == M){
-                System.out.println(p.time);
+            if(cur[0] == M){
+                System.out.println(cur[1]);
                 return;
             }
-            int next1 = p.pos + 1;
-            if(next1 <= v.length-1 && v[next1] > p.time+1){
-                q.offer(new Pos(next1, p.time+1));
-            }
-            int next2 = p.pos - 1;
-            if(next2 >= 0 && v[next2] > p.time+1) {
-                q.offer(new Pos(next2, p.time+1));
-            }
-            int next3 = p.pos * 2;
-            if(next3 <= v.length-1 && v[next3] > p.time){
-                q.offer(new Pos(next3, p.time));
+            int next1 = cur[0] * 2;
+            int next2 = cur[0] + 1;
+            int next3 = cur[0] - 1;
+
+            if(next1 <= 100000 && v[next1] > cur[1]){
+                v[next1] = cur[1];
+                dq.offerFirst(new int[]{next1, cur[1]});
             }
 
+            if(next2 <= 100000 && v[next2] > cur[1] + 1){
+                v[next2] = cur[1] + 1;
+                dq.offerLast(new int[]{next2, cur[1] + 1});
+            }
+            if(next3 >= 0 && v[next3] > cur[1] + 1){
+                v[next3] = cur[1] + 1;
+                dq.offerLast(new int[]{next3, cur[1] + 1});
+            }
 
         }
-
     }
+
 }
